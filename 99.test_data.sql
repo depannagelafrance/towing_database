0c66dfd0b7ec04eb3333bb786dc933f138a1862f
Kris Vandermast <kris.vandermast@ext.ec.europa.eu>
2014-09-18 19:27:19 +0200

    user roles and modules

--- 99.test_data.sql
+++ 99.test_data.sql
@@ -13,19 +13,6 @@ values(1, 1, 'admin', 'Master', 'Chief', 'kvandermast@gmail.com', 1, 0, null, no
 INSERT INTO `T_USER_PASSWORDS` (`id`, `user_id`, `pwd`) VALUES (1, 1, PASSWORD('T0w1nG'));
 INSERT INTO `T_USER_TOKENS` (`user_id`, `token`) VALUES (1, 'TOKEN1');
 
-INSERT INTO `P_ROLES` (`id`, `code`, `name`) VALUES 
-	(1, 'ADMIN', 'Algemeen beheerder'),
-	(2, 'COMPANY_ADMIN', 'Beheerder'),
-	(3, 'FAST_DISPATCH', 'F.A.S.T. Dispatch'),
-	(4, 'FAST_MANAGER', 'F.A.S.T. Dossier beheerder');
-
-
-INSERT INTO `P_MODULES` (`id`, `code`, `name`) VALUES
-	(1, 'FAST_DISPATCH', 'F.A.S.T. Dispatch'),
-	(2, 'FAST_DOSSIER', 'F.A.S.T. Dossierbeheer'),
-	(3, 'ADMIN', 'Algemeen beheer'),
-	(4, 'COMPANY_ADMIN', 'Beheer');
-
 INSERT INTO `P_MODULE_ROLES` (`P_ROLES_id`, `P_MODULES_id`) 
 	VALUES (1, 3), (2, 4), (3, 1), (4, 2);
 
