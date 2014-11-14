4141c71487efb37523ce31d89ac17dcab3f58e4d
Kris Vandermast <kris.vandermast@ext.ec.europa.eu>
2014-11-14 09:01:49 +0100

    some bugfixes

--- 50.03.dossier_routines.sql
+++ 50.03.dossier_routines.sql
@@ -1274,12 +1274,13 @@ BEGIN
 	
 	DECLARE no_rows_found BOOLEAN DEFAULT FALSE;
 	
-	DECLARE c CURSOR FOR 	SELECT d.id as dossier_id, tv.id as towing_voucher_id, d.timeframe_id,
-								  (TIMESTAMPDIFF(MINUTE, tv.signa_arrival, now()) - 60) as extra_time_signa
-							FROM T_DOSSIERS d, T_TOWING_VOUCHERS tv
-							WHERE d.id = tv.dossier_id
-								AND tv.signa_arrival IS NOT NULL
-								AND TIMESTAMPDIFF(MINUTE, tv.signa_arrival, now()) > 60;
+	DECLARE c CURSOR FOR 	SELECT 	d.id as dossier_id, tv.id as towing_voucher_id, d.timeframe_id,
+									(TIMESTAMPDIFF(MINUTE, tv.signa_arrival, now()) - 60) as extra_time_signa
+							FROM 	T_DOSSIERS d, T_TOWING_VOUCHERS tv
+							WHERE 	d.id = tv.dossier_id
+									AND tv.signa_arrival IS NOT NULL
+									AND tv.towing_completed IS NULL
+									AND TIMESTAMPDIFF(MINUTE, tv.signa_arrival, now()) > 60;
 								  
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET no_rows_found = TRUE;
 	
@@ -1313,11 +1314,12 @@ BEGIN
 	
 	DECLARE no_rows_found BOOLEAN DEFAULT FALSE;
 	
-	DECLARE c CURSOR FOR 	SELECT d.id as dossier_id, tv.id as towing_voucher_id, d.timeframe_id,
-								  (TIMESTAMPDIFF(MINUTE, tv.signa_arrival, now()) - 60) as extra_time_accident
-							FROM T_DOSSIERS d, T_TOWING_VOUCHERS tv, P_INCIDENT_TYPES p
-							WHERE d.id = tv.dossier_id
-								AND d.incident_type_id = p.id AND p.code='ONGEVAL';
+	DECLARE c CURSOR FOR 	SELECT 	d.id as dossier_id, tv.id as towing_voucher_id, d.timeframe_id,
+									(TIMESTAMPDIFF(MINUTE, tv.signa_arrival, now()) - 60) as extra_time_accident
+							FROM 	T_DOSSIERS d, T_TOWING_VOUCHERS tv, P_INCIDENT_TYPES p
+							WHERE 	d.id = tv.dossier_id
+									AND d.incident_type_id = p.id AND p.code='ONGEVAL'
+									AND tv.towing_completed IS NULL;
 								  
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET no_rows_found = TRUE;
 	
