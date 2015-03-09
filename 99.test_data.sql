4468c92188f6c379d0c6ada15a10b328ca2e5b77
Kris Vandermast <kris.vandermast@ext.ec.europa.eu>
2015-03-09 09:34:53 +0100

    fixes

--- 99.test_data.sql
+++ 99.test_data.sql
@@ -9,34 +9,34 @@ TRUNCATE TABLE T_USER_PASSWORDS;
 TRUNCATE TABLE T_USER_TOKENS;
 
 INSERT INTO T_USERS(id, company_id, login, first_name, last_name, email, is_active, is_locked, locked_ts, cd, cd_by)
-values(1, 1, 'admin', 'Master', 'Chief', 'kvandermast@gmail.com', 1, 0, null, now(), 'SYSTEM');
+values(1, 1, 'admin', 'Admin', 'Depannage La France', 'assistance@depannagelafrance.be', 1, 0, null, now(), 'SYSTEM');
 INSERT INTO `T_USER_PASSWORDS` (`id`, `user_id`, `pwd`) VALUES (1, 1, PASSWORD('T0w1nG'));
 INSERT INTO `T_USER_TOKENS` (`user_id`, `token`) VALUES (1, 'TOKEN1');
 
-INSERT INTO T_USERS(id, company_id, login, first_name, last_name, email, is_active, is_locked, locked_ts, cd, cd_by)
-values(2, 1, 'gert', 'Gert', 'VDB', 'kvandermast@gmail.com', 1, 0, null, now(), 'SYSTEM');
-INSERT INTO `T_USER_PASSWORDS` (`id`, `user_id`, `pwd`) VALUES (2, 2, PASSWORD('gert'));
+-- INSERT INTO T_USERS(id, company_id, login, first_name, last_name, email, is_active, is_locked, locked_ts, cd, cd_by)
+-- values(2, 1, 'gert', 'Gert', 'VDB', 'kvandermast@gmail.com', 1, 0, null, now(), 'SYSTEM');
+-- INSERT INTO `T_USER_PASSWORDS` (`id`, `user_id`, `pwd`) VALUES (2, 2, PASSWORD('gert'));
 
-INSERT INTO T_USERS(id, company_id, login, first_name, last_name, email, is_active, is_locked, locked_ts, cd, cd_by)
-values(3, 1, 'misja', 'Misja', 'W', 'kvandermast@gmail.com', 1, 0, null, now(), 'SYSTEM');
-INSERT INTO `T_USER_PASSWORDS` (`id`, `user_id`, `pwd`) VALUES (3, 3, PASSWORD('misja'));
+-- INSERT INTO T_USERS(id, company_id, login, first_name, last_name, email, is_active, is_locked, locked_ts, cd, cd_by)
+-- values(3, 1, 'misja', 'Misja', 'W', 'kvandermast@gmail.com', 1, 0, null, now(), 'SYSTEM');
+-- INSERT INTO `T_USER_PASSWORDS` (`id`, `user_id`, `pwd`) VALUES (3, 3, PASSWORD('misja'));
 
-INSERT INTO T_USERS(id, company_id, login, first_name, last_name, email, is_active, is_locked, locked_ts, cd, cd_by)
-values(4, 1, 'kris', 'Kris', 'VDM', 'kvandermast@gmail.com', 1, 0, null, now(), 'SYSTEM');
-INSERT INTO `T_USER_PASSWORDS` (`id`, `user_id`, `pwd`) VALUES (4, 4, PASSWORD('kris'));
+-- INSERT INTO T_USERS(id, company_id, login, first_name, last_name, email, is_active, is_locked, locked_ts, cd, cd_by)
+-- values(4, 1, 'kris', 'Kris', 'VDM', 'kvandermast@gmail.com', 1, 0, null, now(), 'SYSTEM');
+-- INSERT INTO `T_USER_PASSWORDS` (`id`, `user_id`, `pwd`) VALUES (4, 4, PASSWORD('kris'));
 
 
 INSERT INTO `T_USER_ROLES` (`role_id`, `user_id`)
 SELECT id, '1' FROM P_ROLES;
 
-INSERT INTO `T_USER_ROLES` (`role_id`, `user_id`)
-SELECT id, '2' FROM P_ROLES;
+-- INSERT INTO `T_USER_ROLES` (`role_id`, `user_id`)
+-- SELECT id, '2' FROM P_ROLES;
 
-INSERT INTO `T_USER_ROLES` (`role_id`, `user_id`)
-SELECT id, '3' FROM P_ROLES;
+-- INSERT INTO `T_USER_ROLES` (`role_id`, `user_id`)
+-- SELECT id, '3' FROM P_ROLES;
 
-INSERT INTO `T_USER_ROLES` (`role_id`, `user_id`)
-SELECT id, '4' FROM P_ROLES;
+-- INSERT INTO `T_USER_ROLES` (`role_id`, `user_id`)
+-- SELECT id, '4' FROM P_ROLES;
 
 INSERT INTO `T_COMPANY_DEPOTS`(`id`,`company_id`,`name`,`street`,`street_number`,`street_pobox`,`zip`,`city`)
 VALUES (1,1,'DEPOT LA FRANCE','Street','123',null,'2000','Antwerpen');
