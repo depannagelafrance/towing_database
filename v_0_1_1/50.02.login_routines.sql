b73b146551fc63a213dd7f7357e78b44ae7762e6
Kris Vandermast <kvandermast@gmail.com>
2015-11-09 15:26:53 +0100

    added invoice_to

---
+++ v_0_1_1/50.02.login_routines.sql
@@ -0,0 +1,208 @@
+DELIMITER $$
+-- ---------------------------------------------------------------------
+-- DROP ROUTINES
+-- ---------------------------------------------------------------------
+DROP PROCEDURE IF EXISTS R_LOGIN $$
+DROP PROCEDURE IF EXISTS R_LOGIN_TOKEN $$
+DROP PROCEDURE IF EXISTS R_RESOLVE_ACCOUNT_INFO $$
+DROP PROCEDURE IF EXISTS R_UPDATE_USER_PASSWORD $$
+
+DROP PROCEDURE IF EXISTS R_NOT_AUTHORIZED $$
+DROP PROCEDURE IF EXISTS R_NOT_FOUND $$
+
+DROP FUNCTION IF EXISTS F_RESOLVE_LOGIN $$
+
+DROP TRIGGER IF EXISTS TRG_AI_USER_TOKEN $$
+DROP TRIGGER IF EXISTS TRG_AU_USER_TOKEN $$
+
+-- ---------------------------------------------------------------------
+-- CREATE ROUTINES
+-- ---------------------------------------------------------------------
+CREATE PROCEDURE R_NOT_AUTHORIZED()
+BEGIN
+	SELECT 'NOT_AUTHORIZED' as error, 403 as statusCode;
+END $$
+
+CREATE PROCEDURE R_NOT_FOUND()
+BEGIN
+	SELECT 'NOT_FOUND' as error, 404 as statusCode;
+END $$
+
+CREATE PROCEDURE R_RESOLVE_ACCOUNT_INFO(IN p_token VARCHAR(255), OUT o_user_id VARCHAR(36), OUT o_company_id BIGINT)
+BEGIN
+	DECLARE v_user_id VARCHAR(36);
+	DECLARE v_company_id BIGINT;
+
+	SELECT 	u.id, c.id
+	INTO 	v_user_id, v_company_id
+	FROM 	T_USER_TOKENS ut, T_USERS u, T_COMPANIES c
+	WHERE	`token` = p_token
+			AND ut.user_id = u.id
+			AND u.company_id = c.id
+	LIMIT 0,1;
+
+	IF v_user_id IS NOT NULL AND v_company_id IS NOT NULL THEN
+		SET o_user_id = v_user_id;
+		SET o_company_id = v_company_id;
+	END IF;
+END $$
+
+CREATE FUNCTION F_RESOLVE_LOGIN(p_user_id VARCHAR(36), p_token VARCHAR(255)) RETURNS VARCHAR(255)
+BEGIN
+	DECLARE v_login VARCHAR(255);
+
+	SELECT 	login INTO v_login
+	FROM 	T_USERS u, T_USER_TOKENS ut
+	WHERE 	u.id = p_user_id
+			AND u.id = ut.user_id
+			AND ut.token = p_token;
+
+	RETURN v_login;
+END $$
+
+CREATE PROCEDURE R_LOGIN(IN p_login VARCHAR(255), IN p_pwd VARCHAR(255))
+BEGIN
+	DECLARE v_id 		VARCHAR(36);
+	DECLARE v_token 	VARCHAR(255);
+	DECLARE v_is_locked, v_login_attempts TINYINT;
+
+	-- check if account is locked
+	SELECT 	`id` INTO v_id
+	FROM 	T_USERS u
+	WHERE	lower(u.login) = lower(p_login)
+			AND is_locked = 1
+	LIMIT 	0,1;
+
+	IF v_id IS NOT NULL THEN
+		SELECT 'ACCOUNT_LOCKED' as error, 403 as statusCode;
+	ELSE
+		-- it is not locked, did you provide the correct pwd?
+		SELECT 	u.`id`, SHA1(CONCAT(UUID(), u.id, login, now())) as `token`
+		INTO	v_id, v_token
+		FROM  	T_USERS u, T_USER_PASSWORDS up
+		WHERE	lower(u.login) = lower(p_login)
+				AND u.id = up.user_id
+				AND u.is_active = 1 AND dd IS NULL
+				AND up.pwd = PASSWORD(p_pwd)
+		LIMIT 	0,1;
+
+		IF v_id IS NOT NULL AND v_token IS NOT NULL THEN
+			SELECT 	`id` INTO v_id
+			FROM 	T_USERS u
+			WHERE	id = v_id
+					AND is_locked = 0
+			LIMIT 	0,1;
+
+			IF v_id IS NULL THEN
+				SELECT 'ACCOUNT_LOCKED' as error, 403 as statusCode;
+			ELSE
+				INSERT INTO `T_USER_TOKENS` (`user_id`, `token`) VALUES (v_id, v_token)
+				ON DUPLICATE KEY UPDATE token = v_token;
+
+				SELECT  `id`, `login`, `first_name`, `last_name`, v_token AS `token`
+				FROM	T_USERS u
+				WHERE	id = v_id;
+
+				-- RESET THE LOGIN ATTEMPT COUNTER
+				UPDATE 	T_USERS
+				SET 	login_attempts = 0, 
+						is_locked = 0,
+						locked_ts = null
+				WHERE	id = v_id AND dd IS NULL
+				LIMIT	1; 
+			END IF;
+		ELSE
+			-- FAILED LOGIN, so update the login attempts
+			SELECT 	id, IFNULL(login_attempts, 0), v_is_locked INTO v_id, v_login_attempts, v_is_locked
+			FROM 	T_USERS
+			WHERE 	lower(login) = lower(p_login) AND dd IS NULL
+			LIMIT	0,1;
+
+			IF v_id IS NOT NULL THEN
+				UPDATE 	T_USERS
+				SET 	login_attempts = v_login_attempts + 1, 
+						is_locked = IF(v_login_attempts + 1 >= 5, 1, 0),
+						locked_ts = IF(v_login_attempts + 1 >= 5, now(), NULL)
+				WHERE	id = v_id AND dd IS NULL
+				LIMIT	1; 
+			END IF;
+
+			CALL R_NOT_AUTHORIZED();
+		END IF;
+	END IF;
+END $$
+
+
+CREATE PROCEDURE R_LOGIN_TOKEN(IN p_token VARCHAR(255))
+BEGIN
+	DECLARE v_id VARCHAR(36);
+	DECLARE v_token VARCHAR(255);
+
+	SELECT 	u.`id`
+	INTO	v_id
+	FROM  	T_USERS u, T_USER_TOKENS ut
+	WHERE	ut.token = p_token
+			AND u.id = ut.user_id
+			AND u.is_active = 1
+	LIMIT 0,1;
+
+	IF v_id IS NOT NULL THEN
+		SELECT 	`id` INTO v_id
+		FROM 	T_USERS u
+		WHERE	id = v_id
+				AND is_locked = 0;
+
+		IF v_id IS NULL THEN
+			SELECT 'ACCOUNT_LOCKED' as error, 403 as statusCode;
+		ELSE
+			SELECT 	SHA1(CONCAT(UUID(), u.id, login, now())) as `token` INTO v_token
+			FROM 	T_USERS u
+			WHERE   u.id = v_id;
+
+			UPDATE T_USER_TOKENS SET token = v_token WHERE user_id = v_id LIMIT 1;
+
+			SELECT  `id`, `login`, `first_name`, `last_name`, v_token AS `token`
+			FROM	T_USERS u
+			WHERE	id = v_id;
+		END IF;
+	ELSE
+		SELECT 'NOT_AUTHORIZED' as error, 403 as statusCode;
+	END IF;
+END $$
+
+CREATE PROCEDURE R_UPDATE_USER_PASSWORD(IN p_login VARCHAR(255), IN p_password VARCHAR(255), IN p_token VARCHAR(255))
+BEGIN 
+	DECLARE v_company_id BIGINT;
+	DECLARE v_user_id VARCHAR(36);
+
+	CALL R_RESOLVE_ACCOUNT_INFO(p_token, v_user_id, v_company_id);
+
+	IF v_user_id IS NULL OR v_company_id IS NULL THEN
+		CALL R_NOT_AUTHORIZED;
+	ELSE
+		UPDATE T_USER_PASSWORDS
+        SET pwd = PASSWORD(p_password)
+        WHERE user_id = v_user_id
+        LIMIT 1;
+        
+        SELECT "OK" as result;
+    END IF;
+END $$
+
+-- ---------------------------------------
+-- TRIGGERS 
+-- ---------------------------------------
+
+CREATE TRIGGER `TRG_AI_USER_TOKEN` AFTER INSERT ON `T_USER_TOKENS`
+FOR EACH ROW
+BEGIN
+	CALL R_ADD_USER_TOKEN_AUDIT_LOG(NEW.user_id, NEW.token);
+END $$
+
+CREATE TRIGGER `TRG_AU_USER_TOKEN` AFTER UPDATE ON `T_USER_TOKENS`
+FOR EACH ROW
+BEGIN
+	CALL R_ADD_USER_TOKEN_AUDIT_LOG(NEW.user_id, NEW.token);
+END $$
+
+DELIMITER ;
